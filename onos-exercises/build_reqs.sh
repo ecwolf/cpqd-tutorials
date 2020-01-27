9cf3ef45863f04228bd4dd770293d0299af9ae20
Raphael V. Rosa <raphaelvrosa@gmail.com>
2020-01-27 16:56:55 -0300

    Adds builds reqs for OSM
    Signed-off-by: Raphael V. Rosa <raphaelvrosa@gmail.com>

---
+++ onos-exercises/build_reqs.sh
@@ -0,0 +1,4 @@
+apt install mininet
+
+docker pull onosproject/onos
+
---
+++ osm-exercises/build_reqs.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/containernet/vim-emu
+
+wget https://osm-download.etsi.org/ftp/osm-7.0-seven/install_osm.sh
+chmod +x install_osm.sh
+./install_osm.sh --vimemu 2>&1 | tee osm_install_log.txt
+
