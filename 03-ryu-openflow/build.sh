1264909e6df08735058f05ccd74ed4b36b7ce404
Raphael V. Rosa <raphaelvrosa@gmail.com>
2020-01-31 13:18:44 -0300

    Adds folders with requirements to install and run labs
    Signed-off-by: Raphael V. Rosa <raphaelvrosa@gmail.com>

---
+++ 03-ryu-openflow/build.sh
@@ -0,0 +1,7 @@
+sudo apt install gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
+
+git clone https://github.com/osrg/ryu
+
+cd ryu 
+sudo pip3 setup.py install
+cd -
---
+++ 05-onos-exercises/build_reqs.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/mininet/mininet
+
+sudo apt install mininet
+
+docker pull onosproject/onos
+
